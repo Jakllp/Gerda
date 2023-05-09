6b2d5b13fcb7c0dc1d17a5e3e73318e4425695a6
Thyrexx <henry.russxy@gmail.com>
2023-05-09 16:06:20 +0200

    changed the shooter component to hold the packed scene for the projectile instead of the gun

--- resources/equipment/weapon/gun.gd
+++ resources/equipment/weapon/gun.gd
@@ -2,8 +2,6 @@ extends Weapon
 
 class_name Gun
 
-@export var projectile_scene :PackedScene
-
 func update(player: Player) -> void:
 	for child in get_children():
 		if child.has_method("update"):
@@ -11,4 +9,4 @@ func update(player: Player) -> void:
 			
 
 func act(player: Player) -> void:
-	$Shooter.shoot(projectile_scene.instantiate())
+	$Shooter.shoot()
