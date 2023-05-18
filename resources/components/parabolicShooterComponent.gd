e1305b74986129eefb5a356c6e4896b272806363
Thyrexx <henry.russxy@gmail.com>
2023-05-18 18:43:51 +0200

    finished parabolic projectile #28

--- resources/components/parabolicShooterComponent.gd
+++ resources/components/parabolicShooterComponent.gd
@@ -4,102 +4,48 @@ class_name ParabolicShooter
 
 @export var projectile_scene: PackedScene
 var number_of_points = 50.0
-#speed
-var v: float = 100
-#gravity
-var g: float = 10.0
+var target: Vector2
+var velocity: float = 20
+var gravity: float = 10.0
+
 #at which point of the track is the maximum height reached
 var peak_ratio
-# sinus of the alpha angle
-var sa
 
-func _ready():
-	position = Vector2(600,300)
-
-func _physics_process(delta) -> void:
-	if Input.is_action_just_pressed("LMB"):
-		shoot(get_local_mouse_position())
+#angle from x axis to starting velocity vector
+var alpha
 
 func shoot(target: Vector2) -> void:
-	print("shoot: ", target)
-	var projectile: ParabolicProjectile = projectile_scene.instantiate()
-	print("pos: ", position)
-	projectile.position = Vector2.ZERO
-	var res = calculate_trajectory(target)
-	projectile.curve = res[0]
-	projectile.peak_ratio = res[1] / number_of_points
-	projectile.distance = target.length()
-	self.add_child(projectile)
+	print("target: ", target)
+	self.target = target
+	var p: ParabolicProjectile = projectile_scene.instantiate()
+	p.curve = calculate_trajectory2()
+	p.peak_ratio = peak_ratio
+	add_child(p)
 	
+func calculate_trajectory2() -> Curve2D:
+	var dot = Vector2.RIGHT.dot(target.normalized())
+	alpha = PI/2 - PI/4 * dot	
 	
-func calculate_trajectory(target: Vector2) -> Array:
-	var curve: Curve2D = Curve2D.new()
-	
-	var DOT = Vector2(1,0).dot(target.normalized())
-	var angle = 90 - 45 * DOT
-	print("angle: ", angle)
-	
-	var x_dist = target.x
-	var y_dist = -1.0 * target.y
-	
-	var speed = sqrt(((0.5 * g * x_dist * x_dist) / pow(cos(deg_to_rad(angle)), 2.0)) / (y_dist - tan(deg_to_rad(angle)) * x_dist))
-	var x_component = cos(deg_to_rad(angle)) * speed
-	var y_component = sin(deg_to_rad(angle)) * speed
-	print("speed: ", speed)
-	print("x_comp: ", x_component)
-	print("y_comp: ", y_component)
-	
-	var total_time = x_dist / x_component
-	print("total_time: ", total_time)
-	
-	var line: Line2D = Line2D.new()
-	
-	var max_y = 0
-	var max_point: float = 0.0
-	for point in number_of_points:
-		var time = total_time * (point / number_of_points)
-		var dx = time * x_component
-		var dy = -1.0 * (time * y_component + 0.5 * g * time * time)
-#		print("point: ", point)
-#		print("time: ", time)
-#		print("dx: ", dx)
-#		print("dy: ", dy)
-#		print()
-		if dy < max_y:
-			print("new max: ", dy)
-			max_point = point
-		curve.add_point(Vector2(dx,dy))
-		line.add_point(Vector2(dx,dy))
-	
-	add_child(line)
-	return [curve, max_point]
-	
-func calculate_trajectory2(ta: Vector2) -> Curve2D:
-	#angle from x axis to target
-	var beta = acos(Vector2(1,0).dot(ta.normalized()) / (ta.length()))
-	#angle from x axis to starting velocity vector
-	var alpha = 90 - abs(beta)
-	sa = sin(alpha)
-	
-	#maybe + instead of -
-	var help = sqrt(v*v*sa - 2*g*ta.y) / (v * sa)
-	#at which point of the track is the maximum height reached
+	velocity = sqrt((gravity * pow(target.x,2)) / (2 * pow(cos(alpha),2) * (tan(alpha) * target.x + target.y)))
+
+	var help = sqrt(velocity*velocity*sin(alpha) + 2*gravity*abs(target.y)) / (velocity * sin(alpha))
 	peak_ratio = 1/(1+help)
 	
 	var curve = Curve2D.new()
 	
-	var dx: float
-	var dy: float
-	var t: float
+	var x: float = 0
+	var y: float = 0
+	var point = Vector2(x, y)
+	curve.add_point(point)
 	for p in number_of_points:
-		t = t_of_p(p)
+		x = p/number_of_points * target.x
+		y = y_of_x(x)
+		point = Vector2(x,y)
 		
+		curve.add_point(point)
+	
 	return curve
 	
-func y_of_t(t:float) -> float:
-	var p1: float = v * sa * t
-	var p2: float = 0.5 * g * pow(t,2)
-	return p1 - p2
+func y_of_x(x:float) -> float:
+	return -1.0 * x * (tan(alpha) - ((gravity * x) / (2 * pow(cos(alpha),2) * velocity*velocity)))
 	
-func t_of_p(p:int) -> float:
-	return pow((p/number_of_points) - peak_ratio, 2)
--- resources/equipment/weapon/parabolicProjectile.gd
+++ resources/equipment/weapon/parabolicProjectile.gd
@@ -2,47 +2,22 @@ extends Path2D
 
 class_name ParabolicProjectile
 
-var peak_ratio: float = 0
-var distance: float = 0
+@export var speed: int = 15
+@onready var follow: PathFollow2D = $PathFollow2D
 
-func _ready():
-	
-	pass
-	
+var peak_ratio: float
+var parabel_fac: float
 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta) -> void:
-#	var x_ratio = Vector2(1,0).dot(target.normalized())
-#	var angle: float = PI/2 - PI/4 * x_ratio
-#	var flight_time: float = (speed * sin(angle) + sqrt(pow(speed,2) * pow(sin(angle),2) + 2 * gravity * target.y))
-#	var ascend_time: float = (speed * sin(angle)) / gravity
-#	var descend_time = flight_time - ascend_time
-#	var height: float = (pow(speed,2) * pow(sin(angle),2)) / (2 * gravity)
-#	var max_point: Vector2 = Vector2(cos(angle)*speed*ascend_time, height)
-#	var tween: Tween = self.create_tween()
-#	tween.tween_property(self, "position", max_point, ascend_time)
-#	tween.tween_property(self, "position", target, descend_time)
-#
-#	set_physics_process(false)
-#
-#	return
+	follow.progress_ratio += x_of_p()
+	if follow.progress_ratio == 1:
+		release()
 	
-	var d_prog = (distance/5000) * pow($PathFollow2D.progress_ratio - peak_ratio, 2)
+	
+func x_of_p() -> float:
+	return pow((follow.progress_ratio - peak_ratio), 2) * 1/speed + 0.007
 
-	if d_prog <= delta:
-		d_prog += delta
 
-	$PathFollow2D.progress_ratio += d_prog
-	if Input.is_action_just_pressed("RMB"):
-#		print("progress: ", $PathFollow2D.progress_ratio)
-#		print("glob: ", $PathFollow2D.global_position)
-#		print("pos: ", $PathFollow2D.position, "\n")
-#
-#		print("distance/100: ", distance/100)
-#		print(peak_ratio)
-#		print("a: ", pow($PathFollow2D.progress_ratio - peak_ratio, 2))
-		print("d_prog: ", d_prog)
-		
-		print()
+func release() -> void:
+	queue_free()
