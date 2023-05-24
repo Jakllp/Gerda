a22171934761a868fd6b71cdbda25cb51c6a2a55
Thyrexx <henry.russxy@gmail.com>
2023-05-24 21:12:28 +0200

    fixed bug: increase of spider attack speed value decreased the actual speed

--- resources/entity/enemy/spider/spider.gd
+++ resources/entity/enemy/spider/spider.gd
@@ -17,5 +17,5 @@ func attack() -> void:
 	if attack_tween:
 		attack_tween.kill()
 	attack_tween = self.create_tween()
-	attack_tween.tween_property(self, "position", jump_to, attack_speed).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	attack_tween.tween_property(self, "position", jump_to, 1/attack_speed).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 	attack_tween.tween_property(self, "position", jump_back_to, attack_cooldown).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
