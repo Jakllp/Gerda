d87e39dcb7d3efa5c0b2b1dc5a53107cd1edf392
Jakl <57708725+Jakllp@users.noreply.github.com>
2023-05-25 12:24:30 +0200

    Add Ammo-System #24

--- resources/equipment/weapon/gun.gd
+++ resources/equipment/weapon/gun.gd
@@ -2,13 +2,54 @@ extends Weapon
 
 class_name Gun
 
+@export var ammo_needed_per_shot :int
+@export var base_reload_time :int
+@export var max_ammo_stored :int
+@export var mag_size :int
+@onready var mag_contents :int = mag_size
+@onready var ammo_stored :int = max_ammo_stored
+@onready var reload_time :int = base_reload_time
+
+@onready var reload_timer = $ReloadTimer
+
 func update(player: Player) -> void:
 	for child in get_children():
 		if child.has_method("update"):
 			child.update(player)
+			
+	
+	
+	if Input.is_action_just_pressed("reload"):
+		# Do we need to reload and do we have ammo?
+		if mag_contents < mag_size:
+			# If so -> Start reload-process
+			trigger_reload()
+
 
 func act(player: Player, delta: float) -> void:
-	$Shooter.shoot()
+	pass
 
 func do_rotation(player: Player):
 	$Anglepoint.aim_at_mouse_with_right_angled_grip(player)
+
+
+# Starts the Reload-Timer
+func trigger_reload():
+	if ammo_stored > 0:
+		print("Reloading...")
+		reload_timer.wait_time = reload_time
+		reload_timer.start()
+
+
+# Triggered by Reload-Timer
+func reload() -> void:
+	var mag_diff = mag_size - mag_contents
+	if ammo_stored >= mag_diff:
+		# We have enough to fill!
+		mag_contents = mag_size
+		ammo_stored -= mag_diff
+	else: 
+		# Not enough to fill completely -> Empty store
+		mag_contents += ammo_stored
+		ammo_stored = 0
+	print("Reloaded! "+str(mag_contents)+"/"+str(ammo_stored))
