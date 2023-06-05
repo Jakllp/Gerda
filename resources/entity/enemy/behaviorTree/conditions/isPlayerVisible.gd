172182572bc63d9d82d7bbc10146e2b4b26ce11e
Thyrexx <henry.russxy@gmail.com>
2023-06-05 19:57:52 +0200

    fixed visibility check

--- resources/entity/enemy/behaviorTree/conditions/isPlayerVisible.gd
+++ resources/entity/enemy/behaviorTree/conditions/isPlayerVisible.gd
@@ -1,6 +1,7 @@
 extends BTCondition
 
 @export var player_pos_key: String
+@export_flags_2d_physics var collision_mask
 
 func tick(actor:Node, blackboard:BTBlackboard):
 	var player_pos = blackboard.get_data(player_pos_key)
@@ -10,7 +11,7 @@ func tick(actor:Node, blackboard:BTBlackboard):
 	var space_state :PhysicsDirectSpaceState2D = actor.get_world_2d().direct_space_state
 	var query = PhysicsRayQueryParameters2D.create(actor.global_position, player_pos)
 	query.exclude = [actor]
-	query.collision_mask = 0b10000000_00000000_00000000_00000001
+	query.collision_mask = collision_mask
 	var result := space_state.intersect_ray(query)
 	if result.is_empty():
 		return BTTickResult.SUCCESS
