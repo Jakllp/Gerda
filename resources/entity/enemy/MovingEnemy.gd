29d0b47deeca8324ec32297f7bb6ab54b3ec1867
Thyrexx <96577754+Thyrexx@users.noreply.github.com>
2023-06-10 17:28:26 +0200

    Spider movement (#49)
    * better random movement #20
    
    for a more detailed description of how it works see loadWanderPos.gd
    
    * spieder lookaround #20
    
    Everey now and then while wandering around a spider takes a pause and stands still
    Also added more varity to wander distance
    
    * polish #20
    
    * spider will finish path after following player #20

--- resources/entity/enemy/MovingEnemy.gd
+++ resources/entity/enemy/MovingEnemy.gd
@@ -3,12 +3,20 @@ extends CharacterBody2D
 class_name MovingEnemy
 
 ## speed of the enemy
-@export var speed: int = 70
+@export var speed: int = 60
 ## For melee attacks the time it takes to execute the actual attack
 ## For ranged attacks the velocity of the projectile
 @export var attack_speed: float
-##the time between succesive attacks
+## The time between succesive attacks
 @export var attack_cooldown: float
+## The minimum radius for selecting a wander position
+@export var min_wander_target_radius: int = 100
+## The maximum radius for selecting a wander position
+@export var max_wander_target_radius: int = 200
+## The maximum distance this enemy walks on the same wander path
+@export var max_wander_distance: int = 200
+## How much the max_wander_distance can vary
+@export var wander_distance_deviation: int = 100
 
 @onready var sprite :AnimatedSprite2D = $SubViewportContainer/SubViewport/AnimatedSprite2D
 @onready var nav_agent:NavigationAgent2D = $NavigationAgent2D
@@ -16,7 +24,7 @@ class_name MovingEnemy
 func _ready() -> void:
 	sprite.play()
 	nav_agent.radius = ShapeHelper.get_shape_radius($CollisionShape2D.shape)
-	
+	$BTBlackboard.set_data("walked_distance", 0)
 
 func _physics_process(delta: float) -> void:
 	pass
