0cb9c62ff75423d41bca802704e72fb532deb2a8
Jakl <57708725+Jakllp@users.noreply.github.com>
2023-06-14 15:34:14 +0200

    Y-Sort fix

--- resources/entity/player/Player.gd
+++ resources/entity/player/Player.gd
@@ -16,12 +16,12 @@ class_name Player
 @onready var active_upgrades := {}
 
 ## How much the dash increases the movement speed
-const dash_multiplier = 3
-const dash_duration = 0.1
-const dash_max_amount = 2
+const dash_multiplier = 20
+const dash_duration = 0.5
+const dash_max_amount = 50
 var dashes_left = dash_max_amount
 ## How long it takes for dashes to recharge
-var dash_cooldown = 1.0
+var dash_cooldown = 0.1
 
 var input_component = PlayerInputComponent.new()
 
