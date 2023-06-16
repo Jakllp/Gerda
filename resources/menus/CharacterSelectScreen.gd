e4efa181e83dc586b402aa671f94cb17cefe6bfb
leolady02 <130653249+leolady02@users.noreply.github.com>
2023-06-16 16:35:25 +0200

    adjusting string dependencies

--- resources/menus/CharacterSelectScreen.gd
+++ resources/menus/CharacterSelectScreen.gd
@@ -1,12 +1,12 @@
 extends Control
 
 func _on_mole_pressed():
-	get_tree().change_scene_to_file("res://resources/menus/mutator_select_screen.tscn")
+	get_tree().change_scene_to_file("res://resources/menus/MutatorSelectScreen.tscn")
 
 
 func _on_badger_pressed():
-	get_tree().change_scene_to_file("res://resources/menus/mutator_select_screen.tscn")
+	get_tree().change_scene_to_file("res://resources/menus/MutatorSelectScreen.tscn")
 
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://resources/menus/start_menu.tscn")
+	get_tree().change_scene_to_file("res://resources/menus/StartMenu.tscn")
--- resources/menus/SartMenu.gd
+++ resources/menus/SartMenu.gd
@@ -1,15 +1,15 @@
 extends Control
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://resources/menus/character_select_screen.tscn")
+	get_tree().change_scene_to_file("res://resources/menus/CharacterSelectScreen.tscn")
 	
 	
 func _on_options_menu_pressed():
-	get_tree().change_scene_to_file("res://resources/menus/options_menu.tscn")
+	get_tree().change_scene_to_file("res://resources/menus/OptionsMenu.tscn")
 	
 	
 func _on_credits_button_pressed():
-	get_tree().change_scene_to_file("res://resources/menus/credits_screen.tscn")
+	get_tree().change_scene_to_file("res://resources/menus/CreditsScreen.tscn")
 	
 	
 func _on_quit_menu_pressed():
