990c998dbecf1ac2a50042e386e0e65f0ab3d2c5
leolady02 <130653249+leolady02@users.noreply.github.com>
2023-06-18 21:32:38 +0200

    slightly more fancy buttons

--- resources/menus/CharacterSelectScreen.gd
+++ resources/menus/CharacterSelectScreen.gd
@@ -1,12 +1,22 @@
 extends Control
 
-func _on_mole_pressed():
-	get_tree().change_scene_to_file("res://resources/menus/MutatorSelectScreen.tscn")
+@onready var badger_button = $VBoxContainer/HBoxContainer/BadgerButton
 
-
-func _on_badger_pressed():
-	get_tree().change_scene_to_file("res://resources/menus/MutatorSelectScreen.tscn")
-
+func _ready():
+	badger_button.disabled = true
+	
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://resources/menus/StartMenu.tscn")
+
+
+func _on_mole_button_pressed():
+	get_tree().change_scene_to_file("res://resources/menus/MutatorSelectScreen.tscn")
+
+
+func _on_badger_button_pressed():
+	get_tree().change_scene_to_file("res://resources/menus/MutatorSelectScreen.tscn")
+
+
+
+
