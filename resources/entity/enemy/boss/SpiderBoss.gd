4dc9e8e6f7b7e9d84e5248f651a70a8856bdfc3f
Thyrexx <henry.russxy@gmail.com>
2023-06-23 20:54:50 +0200

    thats basically it #63 - introducing Statemachine

--- resources/entity/enemy/boss/SpiderBoss.gd
+++ resources/entity/enemy/boss/SpiderBoss.gd
@@ -1,59 +1,98 @@
 extends StaticBody2D
+class_name SpiderBoss
 
 @export var stomp_react_time: float = 1.5
 @export var stomp_react_time_deviation: float = 0.5
+@export var sweep_react_time: float = 1.5
+@export var sweep_react_time_deviation: float = 0.5
 
 @onready var player: Player = get_tree().get_first_node_in_group("player")
 
+@onready var animation_tree :AnimationTree = $AnimationTree
+
+@onready var shooter: ParabolicShooter = $ParabolicShooter
 @onready var sweep_area: Area2D = $SweepArea
 @onready var stomp_area_left: Area2D = $StompAreaLeft
 @onready var stomp_area_right: Area2D = $StompAreaRight
 @onready var landing_zone_left: CollisionShape2D = $LandingZoneLeft/CollisionShape2D
 @onready var landing_zone_right: CollisionShape2D = $LandingZoneRight/CollisionShape2D
+@onready var stomp_particles_left: GPUParticles2D = $StompParticlesLeft
+@onready var stomp_particles_right: GPUParticles2D = $StompParticlesRight
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var shape_front_leg_left: CollisionShape2D = $CollisionShapeFrontLegLeft
+@onready var shape_front_leg_right: CollisionShape2D = $CollisionShapeFrontLegRight
+
 
 func _ready():
-	sprite.play("idle")
+	animation_tree.active = true
+	$LegLeft.damage = 1
+	$LegRight.damage = 1
+	$HitboxAndBody.damage = 1
 	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func shoot_web() -> void:
-	var player_pos = player.global_position
-	$ParabolicShooter.shoot(player_pos)
+	shooter.shoot(player.global_position)
 	
 
-func _on_timer_timeout():
-	sprite.play("web_attack")
-	await sprite.animation_finished
-	shoot_web()
-	sprite.play("idle")
-
-func stomp(is_left: bool) -> void:
+func stomp_left() -> void:
+	stomp_particles_left.emitting = true
+	if stomp_area_left.get_overlapping_areas().is_empty():
+		return
 	var rand_pos_shift := Vector2(randf_range(-0.5, 0.5), randf_range(-0.5, 0.5))
-	var target_location: Vector2
-	if is_left:
-		target_location = landing_zone_left.global_position + ShapeHelper.get_enclosing_box_size(landing_zone_left.shape) * rand_pos_shift
-		sprite.play("stomp_left")
-	else:
-		target_location = landing_zone_right.global_position + ShapeHelper.get_enclosing_box_size(landing_zone_right.shape) * rand_pos_shift
-		sprite.play("stomp_right")
+	var target_location: Vector2 = landing_zone_left.global_position + ShapeHelper.get_enclosing_box_size(landing_zone_left.shape) * rand_pos_shift
+	yeet_player(target_location)
 	
-	await sprite.animation_finished
-	sprite.play("idle")
+
+func stomp_right() -> void:
+	stomp_particles_right.emitting = true
+	if stomp_area_right.get_overlapping_areas().is_empty():
+		return
+	var rand_pos_shift := Vector2(randf_range(-0.5, 0.5), randf_range(-0.5, 0.5))
+	var target_location: Vector2 = landing_zone_right.global_position + ShapeHelper.get_enclosing_box_size(landing_zone_right.shape) * rand_pos_shift
+	yeet_player(target_location)
+	
+
+func yeet_player(target_location: Vector2) -> void:
 	var tween := create_tween()
 	tween.tween_property(player, "global_position", target_location, 0.4)
 	tween.parallel().tween_property(player, "scale", player.scale * 1.35, 0.2)
 	tween.chain().tween_property(player, "scale", player.scale, 0.2)
 
 
-func _on_stomp_area_area_entered(area, is_left):
+func sweep() -> void:
+	if not sweep_area.get_overlapping_areas().is_empty():
+		animation_tree["parameters/conditions/sweep"] = true
+		get_tree().create_timer(sweep_react_time + randf_range(-sweep_react_time_deviation, sweep_react_time_deviation)).timeout.connect(sweep)
+	else:
+		animation_tree["parameters/conditions/sweep"] = false
+		
+
+func _on_stomp_area_area_entered(_area, is_left):
+	# wait for reaction
 	await get_tree().create_timer(stomp_react_time + randf_range(-stomp_react_time_deviation, stomp_react_time_deviation)).timeout
 	if is_left and stomp_area_left.get_overlapping_areas().is_empty() or !is_left and stomp_area_right.get_overlapping_areas().is_empty():
 		return
+	if is_left:
+		animation_tree["parameters/conditions/stomp_left"] = true
+	else:
+		animation_tree["parameters/conditions/stomp_right"] = true
 	
-	stomp(is_left)
+
+func _on_stomp_area_area_exited(_area, is_left):
+	if is_left:
+		animation_tree["parameters/conditions/stomp_left"] = false
+	else:
+		animation_tree["parameters/conditions/stomp_right"] = false
+		
+
+func _on_shoot_timer_timeout():
+	animation_tree["parameters/conditions/shoot"] = true
+
+
+func _on_sweep_area_area_entered(_area):
+	# wait for reaction
+	get_tree().create_timer(sweep_react_time + randf_range(-sweep_react_time_deviation, sweep_react_time_deviation)).timeout.connect(sweep)
+	
+
+func _on_sweep_area_area_exited(_area):
+	animation_tree["parameters/conditions/sweep"] = false
