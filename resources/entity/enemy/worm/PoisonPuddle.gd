9657428c4d04fa46c25c74e0ab19dda26d9abcce
Jakl <57708725+Jakllp@users.noreply.github.com>
2023-06-26 09:08:37 +0200

    Change poisoned color

--- resources/entity/enemy/worm/PoisonPuddle.gd
+++ resources/entity/enemy/worm/PoisonPuddle.gd
@@ -6,7 +6,7 @@ var containing_bodies: Array
 ## Puts the poisoned status effect on the body inside
 func apply_effect(body) -> void:
 	body.status_effects.add(StatusEffectType.Type.POISONED)
-	body.flash_component.standard_color = Color(0.27,0.95,0.17,1)
+	body.flash_component.standard_color = Color(0.0955,0.9045,0.0,1)
 	body.flash_component.standard_modifier = 0.2
 	body.flash_component.reset_shader_values(body)
 		
