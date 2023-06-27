821e93a3acd975d3c050265d1ad6f34b62dffa77
Jakl <57708725+Jakllp@users.noreply.github.com>
2023-06-27 23:41:24 +0200

    Don't pick up health when already max hp

--- resources/interactables/Item.gd
+++ resources/interactables/Item.gd
@@ -16,12 +16,6 @@ func _on_body_entered(body :Node2D):
 	if body is Player and item_data != null:
 		var player :Player = body
 		
-		var pop_up := pop_up_scene.instantiate()
-		pop_up.get_child(0,false).texture = self.get_child(1, false).texture
-		var pos = player.position - Vector2(0,15)
-		pop_up.global_position = pos
-		get_node("/root/Level/Unshaded").add_child(pop_up)
-		
 		# Handle what the Item actually does
 		if Items.upgrade_category["player"].has(type):
 			player.add_upgrade(type)
@@ -30,11 +24,18 @@ func _on_body_entered(body :Node2D):
 		elif type == Items.Type.ORE:
 			player.ore_pouch += item_data
 		elif type == Items.Type.HEALTH:
-			#TODO what if it no work?
-			player.get_node("PlayerHealthComponent").hp += item_data
+			var helt_component = player.get_node("PlayerHealthComponent")
+			if helt_component.hp == helt_component.hp_max: return
+			helt_component.hp += item_data
 		elif type == Items.Type.HEART:
 			player.get_node("PlayerHealthComponent").hp_max += item_data
 		elif type == Items.Type.DASH:
 			player.dash_max_amount += item_data
-			
+		
+		var pop_up := pop_up_scene.instantiate()
+		pop_up.get_child(0,false).texture = self.get_child(1, false).texture
+		var pos = player.position - Vector2(0,15)
+		pop_up.global_position = pos
+		get_node("/root/Level/Unshaded").add_child(pop_up)
+		
 		queue_free()
