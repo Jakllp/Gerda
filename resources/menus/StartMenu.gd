9ba9eb079ac92eece0d12c9da39c57ceb42717d5
leolady02 <130653249+leolady02@users.noreply.github.com>
2023-06-28 14:19:50 +0200

    minor changes
    - crosshairs
    - buttons in StartMenu can now be disabled (for Demo)

--- resources/menus/StartMenu.gd
+++ resources/menus/StartMenu.gd
@@ -1,5 +1,17 @@
 extends Control
 
+@onready var start_button = $VBoxContainer/StartButton
+@onready var options_button = $VBoxContainer/OptionsMenu
+@onready var credits_button = $VBoxContainer/CreditsButtonnew
+@onready var quit_button = $VBoxContainer/QuitMenu
+
+func _ready():
+	#Disable Buttons for Demo-Version
+	#options_button.disabled = true
+	#quit_button.disabled = true
+	pass
+
+
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://resources/menus/CharacterSelectScreen.tscn")
 	
