71440a0878b26be695e06453e09e671d5711ccdd
Thyrexx <henry.russxy@gmail.com>
2023-06-29 17:49:04 +0200

    better wander behaviour for spider

--- resources/entity/enemy/behaviorTree/actions/LoadWanderPos.gd
+++ resources/entity/enemy/behaviorTree/actions/LoadWanderPos.gd
@@ -21,31 +21,54 @@ const point_tries = 10
 const lambda = 0.005
 
 func tick(actor:Node, blackboard:BTBlackboard):
-	var current_dir = actor.velocity.normalized() if actor.velocity != Vector2.ZERO else Vector2.RIGHT
-	var wander_pos = actor.global_position + current_dir * blackboard.get_data("max_wander_distance")
+	print("----------start load wander pos----------")
+	prints("right:", rad_to_deg(Vector2.RIGHT.angle()))
+	prints("up:", rad_to_deg(Vector2.UP.angle()))
+	prints("left:", rad_to_deg(Vector2.LEFT.angle()))
+	prints("down:", rad_to_deg(Vector2.DOWN.angle()))
+	var current_dir: Vector2 = actor.velocity.normalized() if actor.velocity != Vector2.ZERO else Vector2.RIGHT
+	var rand_dir = randfn(0, PI)
+	var next_dir = current_dir.rotated(rand_dir)
+	var max_wander_distance = blackboard.get_data("max_wander_distance")
+	var actor_pos = actor.global_position
+	var wander_pos = actor_pos + next_dir * randi_range(actor.min_wander_target_radius, actor.max_wander_target_radius)
+	prints("current_dir:", rad_to_deg(current_dir.angle()), "  ---  ", "rand dir:", rad_to_deg(rand_dir), "  ---  ", "next dir:", rad_to_deg(next_dir.angle()))
+	prints("max wander dist:", max_wander_distance, "  ---  ", "actor_pos", actor_pos, "  ---  ", "wander_pos", wander_pos)
 	
 	var space_state :PhysicsDirectSpaceState2D = actor.get_world_2d().direct_space_state
-	var query = PhysicsRayQueryParameters2D.create(actor.global_position, wander_pos)
+	var query = PhysicsRayQueryParameters2D.new()
+	query.from = actor.global_position
+	query.to = wander_pos
 	query.exclude = [actor]
 	query.collision_mask = collision_mask
 	var result := space_state.intersect_ray(query)
 	
 	if not result.is_empty():
+		prints("found result:", result.get("position"))
 		var wall_distance = (result.get("position") - actor.global_position).length()
 		var hold_course_prob = 2 * atan(lambda * wall_distance) / PI
+		prints("wall_distance:", wall_distance, "  ---  ", "hold_prob", hold_course_prob)
 		if randf() < hold_course_prob:
 			wander_pos = result.get("position")
+			prints("hold course with:", wander_pos)
 		else:
-			var i:int = 0
-			var previous_wander_pos = blackboard.get_data(wander_pos_key)
-			while(i < point_tries or wander_pos == previous_wander_pos):
-				i += 1
-				var random_dir = Vector2.from_angle((randf_range(0,1) - randf_range(0,1)) * PI)
-				var random_pos = actor.global_position + random_dir * randi_range(actor.min_wander_target_radius, actor.max_wander_target_radius)
-				wander_pos = NavigationServer2D.map_get_closest_point(actor.get_world_2d().navigation_map,random_pos)
-				if abs(actor.velocity.angle_to(wander_pos - actor.global_position)) < PI/2:
-					break
-			
+			rand_dir = randfn(PI, PI/2)
+			next_dir = current_dir.rotated(rand_dir)
+			var random_pos = actor_pos + next_dir * randi_range(actor.min_wander_target_radius, actor.max_wander_target_radius)
+			query.to = random_pos
+			result = space_state.intersect_ray(query)
+			if result.is_empty():
+				wander_pos = random_pos
+			else:
+				wander_pos = result.get("position")
+			prints("rand pos:", random_pos, "  ---  ", "wander pos:", wander_pos)
+	else:
+		print("no result found")
 	blackboard.set_data(wander_pos_key, wander_pos)
+	prints("set wander pos with:", wander_pos)
+	print("----------end load wander pos----------\n")
 	return BTTickResult.SUCCESS
 	
+func _unhandled_input(event):
+	if event is InputEventKey and event.is_action_pressed("ui_cancel"):
+		get_tree().paused = true
--- resources/entity/enemy/behaviorTree/conditions/IsTargetReached.gd
+++ resources/entity/enemy/behaviorTree/conditions/IsTargetReached.gd
@@ -1,7 +1,8 @@
 extends BTCondition
 
 func tick(actor:Node, blackboard:BTBlackboard):
-	if actor.nav_agent.is_target_reached() :
+	#prints("target dist:", actor.nav_agent.distance_to_target(), " final pos:", actor.nav_agent.get_final_position(), " next papos:", actor.nav_agent.get_next_path_position(), " fin?", actor.nav_agent.is_navigation_finished(), " rea?", actor.nav_agent.is_target_reached())
+	if actor.nav_agent.is_navigation_finished():
 		return BTTickResult.SUCCESS
 	else:
 		return BTTickResult.FAILURE
