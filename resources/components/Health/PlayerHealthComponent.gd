cb69cbb2f6f086ee1e6bc92b658f821d62b2f70b
Thyrexx <henry.russxy@gmail.com>
2023-06-29 11:23:03 +0200

    removed redundancy from PlayerHealthComponent

--- resources/components/Health/PlayerHealthComponent.gd
+++ resources/components/Health/PlayerHealthComponent.gd
@@ -3,20 +3,13 @@ extends SupervisedHealthComponent
 
 func set_max_hp(value):
 	if value != hp_max:
-		var difference :int
-		difference = value - hp_max
+		var difference :int = value - hp_max
 		hp_max = value
 		if difference > 0:
 			self.hp += difference
 		else:
 			self.hp = hp
 		max_hp_changed.emit(value)
-
-func set_hp(value):
-	var previous = hp
-	super.set_hp(value)
-	if previous != hp:
-		hp_changed.emit(previous, hp - previous)
 
 
 func receive_damage(damage: int) -> void:
