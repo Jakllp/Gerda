a175d9a8cb275732f80fa2da53d31808b69fea65
Jakl <57708725+Jakllp@users.noreply.github.com>
2023-07-04 21:00:44 +0200

    Do #150

--- resources/interactables/Trapdoor.gd
+++ resources/interactables/Trapdoor.gd
@@ -3,17 +3,14 @@ extends StaticBody2D
 class_name Trapdoor
 
 
-var open := false
-
 
 func do_interaction() -> void:
-	if not open:
-		# Open the chest - simple enough
-		open = true
-		$Sprite2D.frame = 1
-		$Collision.queue_free()
-		$Collision2.disabled = false
-	else:
-		if Input.is_action_just_pressed("interact"):
-			get_node("/root/Main/GameWorld").proceed_level()
+	# Open the chest - simple enough
+	$Sprite2D.frame = 1
+	$Collision.queue_free()
+	$Collision2.disabled = false
+	$DroppingIn/CollisionShape2D.disabled = false
 
+
+func _on_dropping_in_body_entered(body):
+	get_node("/root/Main/GameWorld").proceed_level()
