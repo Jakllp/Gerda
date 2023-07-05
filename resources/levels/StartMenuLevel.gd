cb70a65aaf9ab1c389810a5d3c7b428a175ab376
Thyrexx <henry.russxy@gmail.com>
2023-07-05 14:39:28 +0200

    fix startmenu camera #152

--- resources/levels/StartMenuLevel.gd
+++ resources/levels/StartMenuLevel.gd
@@ -6,3 +6,4 @@ func _ready():
 	$CanvasModulate.visible = false
 	var tweeny = create_tween()
 	tweeny.tween_property($CanvasModulate, "visible", true, 0.01)
+	$Enemies/SpecialSpider/Camera2D.make_current()
