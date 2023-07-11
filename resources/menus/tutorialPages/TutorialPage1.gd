edafae9732fca53090d565b3a97174fc704a85d8
leolady02 <130653249+leolady02@users.noreply.github.com>
2023-07-11 13:20:30 +0200

    Tutorial pages. #135
    The Button in the start menu does not work yet tho.
    And changes to the text are very welcome.

---
+++ resources/menus/tutorialPages/TutorialPage1.gd
@@ -0,0 +1,10 @@
+extends Control
+
+signal switch_scene(scene: Main.Scene)
+
+func _on_right_button_pressed():
+	get_tree().change_scene_to_file("res://resources/menus/tutorialPages/TutorialPage2.tscn")
+
+
+func _on_back_button_pressed():
+	switch_scene.emit(Main.Scene.START)
---
+++ resources/menus/tutorialPages/TutorialPage2.gd
@@ -0,0 +1,14 @@
+extends Control
+
+signal switch_scene(scene: Main.Scene)
+
+func _on_right_button_pressed():
+	get_tree().change_scene_to_file("res://resources/menus/tutorialPages/TutorialPage3.tscn")
+
+
+func _on_left_button_pressed():
+	get_tree().change_scene_to_file("res://resources/menus/tutorialPages/TutorialPage1.tscn")
+
+
+func _on_back_button_pressed():
+	switch_scene.emit(Main.Scene.START)
