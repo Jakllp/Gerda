58f76f8f496e5b8daf3a8698fc862375fd02b7a6
Jakllp <57708725+Jakllp@users.noreply.github.com>
2023-07-13 14:53:39 +0200

    MoreStone balancing

--- resources/datatypes/Mutator.gd
+++ resources/datatypes/Mutator.gd
@@ -13,7 +13,7 @@ var texture :Vector2
 const modifier_dict = {
 	MutatorType.HARDENED_STONE: 	[1.4,1.8,2.2],
 	MutatorType.LESS_ORE: 			[1.1,1.125,1.15],
-	MutatorType.MORE_STONE: 		[1.25,1.625,2.0],
+	MutatorType.MORE_STONE: 		[1.25,1.5,1.75],
 	MutatorType.SPAWN_PLUS: 		[1, 2, 3],
 	MutatorType.DAMAGE_PLUS: 		[1.0,2.0,3.0],
 	MutatorType.HEALTH_PLUS: 		[1.0,2.0,3.0],
