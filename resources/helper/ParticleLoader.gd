d57b64492137bc0ca9905b36a45c0089da639c31
Jakllp <57708725+Jakllp@users.noreply.github.com>
2023-07-14 17:06:44 +0200

    Preload Particles :)

---
+++ resources/helper/ParticleLoader.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+var materials = [
+	preload("res://resources/particles/DashEffect.tres"),
+	preload("res://resources/particles/biome1/SpiderBossHit.tres"),
+	preload("res://resources/particles/biome1/SpiderBossLegLeft.tres"),
+	preload("res://resources/particles/biome1/SpiderBossLegRight.tres"),
+	preload("res://resources/particles/biome1/SpiderBossStomp.tres"),
+	preload("res://resources/particles/biome1/SpiderHit.tres"),
+	preload("res://resources/particles/biome1/WormHit.tres")
+]
+
+func _ready():
+	for material in materials:
+		var particles_instance = GPUParticles2D.new()
+		particles_instance.process_material = material
+		particles_instance.one_shot = true
+		particles_instance.modulate = Color(1,1,1,0)
+		particles_instance.set_emitting(true)
+		self.add_child(particles_instance)
