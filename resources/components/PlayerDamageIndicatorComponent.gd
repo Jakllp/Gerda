b35e20d9952d1be2305691e2ad13c33cfca3d6ab
Jakl <57708725+Jakllp@users.noreply.github.com>
2023-07-15 01:05:11 +0200

    Fix Vignette on direct poison-hit

--- resources/components/PlayerDamageIndicatorComponent.gd
+++ resources/components/PlayerDamageIndicatorComponent.gd
@@ -6,9 +6,11 @@ var standard_vignette_enabled := false
 var standard_vignette_color := Color(1.0,0,0,1)
 var standard_vignette_softness := 3.0
 
+var reset_tween :Tween = null
+
 ## Handles Screenshake and Vignette, also calls the normal flash
 func flash(le_owner :Player, damage_type :HealthComponent.DamageType):
-	super.flash(le_owner, damage_type)
+	if not is_instance_valid(le_owner): return
 	var vig_node = le_owner.get_node("Camera2D/CanvasLayer/VignetteEffekt")
 	var vig_shader_mat = vig_node.material
 	var tweeny :Tween = le_owner.create_tween()
@@ -24,6 +26,7 @@ func flash(le_owner :Player, damage_type :HealthComponent.DamageType):
 			le_owner.get_node("Camera2D").apply_noise_screen_shake(7.5, 5.0, 1.0, 20.0)
 		_:
 			le_owner.get_node("Camera2D").apply_noise_screen_shake(25, 5.0, 1.0, 30.0)
+	super.flash(le_owner, damage_type)
 	
 
 
@@ -33,18 +36,24 @@ func reset_shader_values(le_owner):
 	if not is_instance_valid(le_owner): return
 	var vig_node = le_owner.get_node("Camera2D/CanvasLayer/VignetteEffekt")
 	var vig_shader_mat = vig_node.material
-	var tweeny :Tween = le_owner.create_tween()
+	
+	if reset_tween != null:
+		reset_tween.stop()
+		reset_tween = null
+		
+	reset_tween = le_owner.create_tween()
 	
 	if(standard_vignette_enabled):
-		tweeny.tween_method(set_vig_softness.bind(le_owner), vig_shader_mat.get_shader_parameter("softness"), self.standard_vignette_softness, 0.15)
+		reset_tween.tween_method(set_vig_softness.bind(le_owner), vig_shader_mat.get_shader_parameter("softness"), self.standard_vignette_softness, 0.15)
 	else:
-		tweeny.tween_method(set_vig_softness.bind(le_owner), vig_shader_mat.get_shader_parameter("softness"), self.standard_vignette_softness, 0.15)
-		tweeny.tween_callback(disable_vig.bind(vig_node))
+		reset_tween.tween_method(set_vig_softness.bind(le_owner), vig_shader_mat.get_shader_parameter("softness"), self.standard_vignette_softness, 0.15)
+		reset_tween.tween_callback(disable_vig.bind(vig_node))
 
 
 func disable_vig(vig_node) -> void:
 	vig_node.visible = false
 	vig_node.material.set_shader_parameter("vignette_color", self.standard_vignette_color)
+	reset_tween = null
 
 
 func set_vig_softness(value :float, le_owner):
